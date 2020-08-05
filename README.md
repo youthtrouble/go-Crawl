7ff6dac4beee6aaf1398fb85cc4f4a8c78b52777
youthtrouble <harjhibolar@gmail.com>
2020-08-05 21:16:30 +0100

    webcrawling works but not as perfectly as can be. see README.md for more info

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # go-Crawl
+
+A Simple webcrawler in Go which uses flags to oarse in a url to crawl for link attributes in the pages HTML.
+
+## To-Add
+[] Support for concatenating indigineous links with the website's base URL.
+[] Twitter API call to tweet new links once with their Headings for indeed.com Job website.
---
+++ crawl.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+	"net/http"
+	"os"
+	"golang.org/x/net/html"
+)
+
+func getBody(url string) {
+	resp, err := http.Get(url) 
+	if err != nil {
+		fmt.Printf("URL %s is invalid", url)
+	}
+	defer resp.Body.Close()
+	anchors := []string{}
+	page := html.NewTokenizer(resp.Body)
+	for {
+		tokenType := page.Next()
+		if tokenType == html.ErrorToken {
+			for _, anchor := range(anchors) {
+				fmt.Println(anchor)
+			}
+		}
+		token := page.Token()
+		if tokenType == html.StartTagToken && token.DataAtom.String() == "a" {
+			for _, KeyVal := range token.Attr {
+				if KeyVal.Key == "href" {
+					fmt.Println(KeyVal.Val)
+				}
+			}
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {   
+		fmt.Println("Please specify start page")  // if a starting page wasn't provided as an argument
+		os.Exit(1)                                // show a message and exit.
+	  }
+	  getBody(args[0])    
+}
