62347ef2b6b33551e980b2d14f5af207995f4618
youthtrouble <harjhibolar@gmail.com>
2020-08-11 11:57:50 +0100

    utilized maps to improve time complexity as well as print only links(keys) not already in the map

--- crawl.go
+++ crawl.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	//"flag"
+	//"os"
 	
 	"golang.org/x/net/html"
 )
@@ -16,30 +18,48 @@ func getBody(url string) {
 		fmt.Printf("URL %s is invalid", url)
 	}
 	defer resp.Body.Close()
-	anchors := []string{}
+
+	anchors := make(map[string]bool)
+
 	page := html.NewTokenizer(resp.Body)
 	for {
 		tokenType := page.Next()
 		if tokenType == html.ErrorToken {
-			for _, anchor := range(anchors) {
-				fmt.Println(anchor)
-			}
+			// for _, anchor := range(anchors) {
+			// 	fmt.Println(anchor)
+			break
+			
 		}
+
 		token := page.Token()
 		if tokenType == html.StartTagToken && token.DataAtom.String() == "a" {
 			for _, KeyVal := range token.Attr {
 				if KeyVal.Key == "href" {
 					if strings.Contains(KeyVal.Val, "/jobs/") {
-						fmt.Println("https://ng.indeed.com" + KeyVal.Val)
+						url := "https://ng.indeed.com" + KeyVal.Val
+						_, exists := anchors[url]
+						if !exists {
+							anchors[url] = true
+						}
 					}
 				}
 			}
 		}
+	}
+	for key := range anchors {
+		fmt.Println(key)
 	}
 }
 
 
 
 func main() {
+	// flag.Parse()
+	// args := flag.Args()
+	// if len(args) < 1 {   
+	// 	fmt.Println("Please specify start page")  // if a starting page wasn't provided as an argument
+	// 	os.Exit(1)                                // show a message and exit.
+	//   }
+	//   getBody(args[0])
 	getBody("https://ng.indeed.com/jobs-in-Lagos")
 }
