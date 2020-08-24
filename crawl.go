272820098b22ee3f1f7f7a4e8d2468080280bdd9
youthtrouble <harjhibolar@gmail.com>
2020-08-24 19:22:35 +0100

    slight typo fix

--- crawl.go
+++ crawl.go
@@ -66,7 +66,7 @@ func getBody(url string, nextURLs map[string]bool) {
 							//if we find a h1 tag, extract the data
 							jobtoken := jobpage.Token()
 							if jobTokentype == html.StartTagToken && token.DataAtom.String() == "h1" {
-								jobInfo{Title: jobtoken.Data}
+								jobtoken = jobInfo{Title: jobtoken.Data}
 
 							}
 						}
